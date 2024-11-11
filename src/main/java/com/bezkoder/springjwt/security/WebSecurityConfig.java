5b19eb52cd55f221f24ed12aa629aa295d23abf3
Anis FETOUI <anis.fetoui@esprit.tn>
2024-11-11 22:01:51 +0100

    modify docker-composePG & security

--- src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
+++ src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.bezkoder.springjwt.security;
 
-import com.bezkoder.springjwt.models.ERole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +11,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -29,6 +30,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
+@EnableWebSecurity
 
 public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
   @Autowired
@@ -68,6 +70,15 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 
 
 
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+            .authorizeRequests(auth ->
+                    auth.anyRequest().permitAll() // Allow all requests without authentication
+            );
+
+    return http.build();
+  }
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable())
@@ -152,5 +163,6 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 
     return source;
   }
+
 
 }
