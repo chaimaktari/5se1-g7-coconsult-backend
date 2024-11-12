d1cf9a521e4273f0c6e43dbcfe477dd4576ef1a5
gramiaziz <gramiaziz9@gmail.com>
2024-11-12 17:21:12 +0100

    fa

--- src/test/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplicationTests.java
+++ src/test/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplicationTests.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class SpringBootSecurityJwtApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+
 
 }
