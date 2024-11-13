21ac14fe106224f21d92e697061ac14ce25a9cfb
Anis FETOUI <anis.fetoui@esprit.tn>
2024-11-13 16:42:14 +0100

    modify the pom.xml

--- src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
+++ src/main/java/com/bezkoder/springjwt/security/WebSecurityConfig.java
@@ -79,6 +79,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 
     return http.build();
   }
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable())
